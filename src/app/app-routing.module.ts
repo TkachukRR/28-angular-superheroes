2f5fc7639a09b21fd7116ac71fa342f37af44f5b
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-16 13:23:41 +0100

    #002 Registered route forLoginPageComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {LoginPageComponent} from "./components/login-page/login-page.component";
 
 const routes: Routes = [
-  {path: '', component: MainLayoutComponent, pathMatch: "full"}
+  {path: '', component: MainLayoutComponent, pathMatch: "full"},
+  {path: 'login', component: LoginPageComponent},
 ];
 
 @NgModule({
