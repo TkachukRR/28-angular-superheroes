4780e02d0d474f78fcf98fb909d174acfdb6dda2
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-16 13:19:11 +0100

    #002 Generated login-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import { HeaderComponent } from './componets/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
