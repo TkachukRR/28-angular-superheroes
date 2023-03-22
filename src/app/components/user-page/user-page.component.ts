9e611a1b462e1026f8fe1f0bbf4e242fb673952f
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-22 14:33:42 +0100

    #003 Created auth.guard.service

--- src/app/components/user-page/user-page.component.ts
+++ src/app/components/user-page/user-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
 	selector: 'app-user-page',
 	templateUrl: './user-page.component.html',
 	styleUrls: ['./user-page.component.scss']
 })
-export class UserPageComponent {}
+export class UserPageComponent {
+  constructor(private auth: AuthService) {
+  }
+  public logout() {
+    this.auth.logout();
+  }
+}
