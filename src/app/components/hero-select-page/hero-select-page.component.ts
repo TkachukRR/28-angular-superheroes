c3810da9611c89354b04ddb6d2a6d200b673cfc4
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-23 21:57:00 +0100

    #004 Added serch form styles

--- src/app/components/hero-select-page/hero-select-page.component.ts
+++ src/app/components/hero-select-page/hero-select-page.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { FormGroup } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-hero-select-page',
   templateUrl: './hero-select-page.component.html',
   styleUrls: ['./hero-select-page.component.scss']
 })
-export class HeroSelectPageComponent {
+export class HeroSelectPageComponent implements OnInit{
+
   public searchForm!: FormGroup;
 
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      searchInput: new FormControl('')
+    });
+  }
 
   public searchHero() {
-
+      console.log(this.searchForm.controls?.['searchInput'].value)
   }
 }
