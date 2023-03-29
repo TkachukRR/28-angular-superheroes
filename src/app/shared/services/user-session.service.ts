0ec634805bc69e34017d9ddbf0d6a5aeb115e4dc
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-29 23:36:03 +0200

    #005 Added logic to User session service

--- src/app/shared/services/user-session.service.ts
+++ src/app/shared/services/user-session.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { RegisteredUser } from "../interfaces";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserSessionService {
+  public activeUser = {};
 
   constructor() { }
+
+  public setActiveUser(user: RegisteredUser): void{
+    this.activeUser = {
+      name: user.name,
+      email: user.email,
+      favourites: user.favourites,
+      selectedHero: user.selectedHero,
+      powerUps: user.powerUps
+    };
+  }
+
+  public getActiveUser(){
+    return this.activeUser;
+  }
 }
