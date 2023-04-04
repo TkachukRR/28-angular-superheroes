675b3ba96d27a57bce2519688a8a8e3a38342e22
Roman Tkachuk <tkachukrr@gmail.com>
2023-04-05 00:31:08 +0200

    #009 Added posability geting full hero information

--- src/app/shared/components/hero-info-page/hero-info-page.component.ts
+++ src/app/shared/components/hero-info-page/hero-info-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces';
 
 @Component({
-  selector: 'app-hero-info-page',
-  templateUrl: './hero-info-page.component.html',
-  styleUrls: ['./hero-info-page.component.scss']
+	selector: 'app-hero-info-page',
+	templateUrl: './hero-info-page.component.html',
+	styleUrls: ['./hero-info-page.component.scss']
 })
-export class HeroInfoPageComponent {
+export class HeroInfoPageComponent implements OnInit {
+	public hero!: Hero;
 
+	constructor(private route: ActivatedRoute, private heroService: HeroesService) {}
+
+	public ngOnInit(): void {
+		this.route.params.subscribe((params: Params) => {
+			this.heroService.getByName(params['heroName'].replace('_', ' ')).subscribe(response => {
+				if (response.response === 'success') {
+					[this.hero] = response['results'];
+					console.log(this.hero);
+				}
+			});
+		});
+	}
 }
